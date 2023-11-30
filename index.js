6f2db36439b2d59d67179133d7306b40a435553a
ludiaz978 <142175671+ludiaz978@users.noreply.github.com>
2023-11-29 19:50:30 -0800

    express

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
+
 //import http from http
-import http from 'http';
+// import http from 'http';
 
-// create a server object using http module
-//req, res are the parameters that give access to the request and response objects
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('Welcome to my server! YAY!\n');
-    res.end(('Hello Friends!\n'));
-    });
-    
-const PORT = 3000;
+// // create a server object using http module
+// //req, res are the parameters that give access 
+// // to the request and response objects
+// const server = http.createServer((req, res) => {
+//     res.writeHead(200, {'Content-Type': 'text/plain'});
+//     res.write('Welcome to my server! YAY!\n');
+//     res.end(('Hello Friends!\n'));
+//     });
+//   //the server object listens on port 3000  
+// const PORT = 3000;
 
-//start the server
-server.listen(PORT, () => {
-    console.log('Server running on port ${PORT}')
+// //start the server
+// //listen for activity on port 3000
+// //callback runs when the server starts listening
+// server.listen(PORT, () => {
+//     console.log('Server running on port ${PORT}')
+// })
+
+//express.js demo
+//import express module
+import express from 'express';
+
+//create an express application
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Home Page!');  
 })
+//define a route handler for the /about page
+//respond with a srtring "About us"
+
+app.get('/about', (req, res) => {
+    res.send('About us');
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+   console.log('Server running on port ${PORT}');
+});
